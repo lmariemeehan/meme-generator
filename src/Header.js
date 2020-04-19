2a3b6fe532c0389a30620582ad154b9e7fc313d8
Lola <lmariemeehan@gmail.com>
2020-04-19 10:00:02 -0700

    Meme generator app for fun and added first header image

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            Meme-Generator
+            <img src="/memeHeaderImg.png" alt="meme face logo"/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/MemeGenerator.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super()
+        this.state= {
+            randImg: ""
+        }
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+    
+}
+
+export default MemeGenerator
