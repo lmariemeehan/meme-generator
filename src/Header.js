2a164c00994e1294d540adc3c09d0703b3334dc4
Lola <lmariemeehan@gmail.com>
2020-04-19 11:13:51 -0700

    Finished with header for now

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
     return (
         <header>
-            Meme-Generator
             <img src="/memeHeaderImg.png" alt="meme face logo"/>
+            <p>Meme-Generator</p>
         </header>
     )
 }
